51c9da355c4306027120cf693e65ce7807f9381d
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-10-03 23:15:56 +0200

    Añado la clase a testear y el proyecto de testing

---
+++ PostPruebasUnitarias/ProcesadorMatematico.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PostPruebasUnitarias
+{
+    public static class ProcesadorMatematico
+    {
+        public static double Sumar(double primerNumero, double segundoNumero)
+        {
+            return primerNumero + segundoNumero;
+        }
+        public static double Resta(double primerNumero, double segundoNumero)
+        {
+            return primerNumero - segundoNumero;
+        }
+        public static double Multiplicacion(double primerNumero, double segundoNumero)
+        {
+            return primerNumero * segundoNumero;
+        }
+        public static double Division(double dividendo, double divisor)
+        {
+            return dividendo / divisor;
+        }
+        public static double Potencia(double numeroBase, double exponente)
+        {
+            return Math.Pow(numeroBase, exponente);
+        }
+        public static double Raiz(double numeroBase, double exponente)
+        {
+            return Math.Pow(numeroBase, 1 / exponente);
+        }
+    }
+}
